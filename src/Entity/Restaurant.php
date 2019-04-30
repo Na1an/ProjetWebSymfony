89f8813dbec2e6ac625bb9ec4a5233ce1efac5c8
Yuchen <yuchenbai@hotmail.com>
2019-04-30 19:30:17 +0200

    footer works; image for one object wroks

--- src/Entity/Restaurant.php
+++ src/Entity/Restaurant.php
@@ -8,10 +8,14 @@ use Doctrine\ORM\Mapping as ORM;
 use Cocur\Slugify\Slugify;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\RestaurantRepository")
  * @UniqueEntity("name")
+ * @Vich\Uploadable()
  */
 class Restaurant  
 {
@@ -31,6 +35,19 @@ class Restaurant
      * @ORM\Column(type="integer")
      */
     private $id;
+
+    /**
+     * @var string|null
+     * @ORM\Column(type="string", length=255)
+     */
+
+    private $filename;
+
+    /**
+     * @var File|null
+     * @Vich\UploadableField(mapping="restaurant_image", fileNameProperty="filename")
+     */
+    private $imageFile;
 
     /**
      * @ORM\Column(type="string", length=255)
@@ -84,6 +101,12 @@ class Restaurant
      * @ORM\ManyToMany(targetEntity="App\Entity\Option", inversedBy="properties")
      */
     private $options;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @var \DateTime|null
+     */
+    private $updated_at;
 
     public function __construct()
     {
@@ -242,6 +265,56 @@ class Restaurant
             $this->options->removeElement($option);
             $option->removeProperty($this);
         }
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getFilename(): ?string
+    {
+        return $this->filename;
+    }
+    /**
+     * @param string|null $filename
+     * @return Restaurant
+     */
+    public function setFilename(?string $filename): Restaurant
+    {
+        $this->filename = $filename;
+        return $this;
+    }
+    /**
+     * @return File|null
+     */
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+    /**
+     * @param File|null $imageFile
+     * @return Restaurant
+     */
+    public function setImageFile(?File $imageFile): Restaurant
+    {
+        $this->imageFile = $imageFile;
+        
+        if($this->imageFile instanceof UploadedFile){
+            $this->updated_at = new \DateTime('now');
+        }
+        
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updated_at): self
+    {
+        $this->updated_at = $updated_at;
 
         return $this;
     }
--- src/Form/RestaurantType.php
+++ src/Form/RestaurantType.php
@@ -9,6 +9,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType; 
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class RestaurantType extends AbstractType
 {
@@ -27,6 +28,9 @@ class RestaurantType extends AbstractType
                 'class' => Option::class,
                 'choice_label' => 'name',
                 'multiple' => true,
+            ])
+            ->add('imageFile', FileType::class, [
+                'required' => false
             ])
             ->add('evaluation', ChoiceType::class, [
                 'choices' => $this->getEvaChoice()
